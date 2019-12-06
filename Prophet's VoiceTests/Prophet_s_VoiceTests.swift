6bf70d64b3da4a3f04656c3fdc0c29fafdb99272
Marc Page <marcallenpage@gmail.com>
2019-12-06 15:31:01 -0600

    First commit

---
+++ Prophet's VoiceTests/Prophet_s_VoiceTests.swift
@@ -0,0 +1,34 @@
+//
+//  Prophet_s_VoiceTests.swift
+//  Prophet's VoiceTests
+//
+//  Created by Marc on 12/6/19.
+//  Copyright © 2019 Resolve To Excel. All rights reserved.
+//
+
+import XCTest
+@testable import Prophet_s_Voice
+
+class Prophet_s_VoiceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
