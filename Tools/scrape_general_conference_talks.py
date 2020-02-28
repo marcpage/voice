efcfc2ae5e02ec785d23878235a26992e2c71eed
Marc Page <marcallenpage@gmail.com>
2020-02-27 19:52:59 -0600

    fix up

--- Tools/scrape_general_conference_talks.py
+++ Tools/scrape_general_conference_talks.py
@@ -6,6 +6,12 @@
 #  Created by Marc on 12/8/19.
 #  Copyright © 2019 Resolve To Excel. All rights reserved.
 
+"""
+If you get a certificate error, see this page:
+
+https://stackoverflow.com/questions/44649449/brew-installation-of-python-3-6-1-ssl-certificate-verify-failed-certificate/44649450#44649450
+"""
+
 # // MARK: - Imports
 
 import hashlib
